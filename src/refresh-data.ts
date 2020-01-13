ca5d34847298d04fbe1bdfb400ccd6a84c3f5a15
Dan Bjorge <dan@dbjorge.net>
2020-01-12 23:53:47 -0800

    Handle data dir not existing

--- src/refresh-data.ts
+++ src/refresh-data.ts
@@ -2,12 +2,15 @@ import * as fs from 'fs';
 import * as https from 'https';
 import * as path from 'path';
 
+const dataDir = path.join(__dirname, '../data');
+
 function download(dataFileName: string, url: string) {
-    const filePath = path.join(__dirname, '../data', dataFileName);
+    const filePath = path.join(dataDir, dataFileName);
     const fileStream = fs.createWriteStream(filePath);
     https.get(url, r => r.pipe(fileStream));
 }
 
+fs.mkdirSync(dataDir, {recursive: true});
 download('skill_gems.json', 'https://poe.ninja/api/data/ItemOverview?league=Metamorph&type=SkillGem');
 download('unique_armour.json', 'https://poe.ninja/api/data/ItemOverview?league=Metamorph&type=UniqueArmour');
 download('unique_jewel.json', 'https://poe.ninja/api/data/ItemOverview?league=Metamorph&type=UniqueJewel');
