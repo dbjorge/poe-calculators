8aefc3046d2f1d30646c3527a3482d546875722b
Dan Bjorge <dan@dbjorge.net>
2025-06-08 18:01:22 -0700

    remove top nav for now

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,33 +1,17 @@
 import * as React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, HashRouter, Link, Route, Routes } from "react-router-dom";
-import styles from "./App.module.css";
 import { RollingMagmaCalculator } from "./RollingMagmaCalculator";
-
-const TopNav: React.FC = () => {
-  return (
-    <nav className={styles.nav}>
-      <ul>
-        <li className={styles.navHome}>
-          <Link to="/">PoE Calculators</Link>
-        </li>
-        <li>
-          <Link to="/rolling-magma">Rolling Magma</Link>
-        </li>
-      </ul>
-    </nav>
-  );
-};
-TopNav.displayName = "TopNav";
 
 const Home: React.FC = () => {
   return (
     <>
-      <h1>PoE Calculators</h1>
-      <p>
-        This is a collection of calculators for Path of Exile. Choose a tool
-        above.
-      </p>
+      <h1><Link to="/">PoE Calculators</Link></h1>
+      <ul>
+        <li>
+          <Link to="/rolling-magma">Rolling Magma</Link>
+        </li>
+      </ul>
     </>
   );
 };
@@ -37,7 +21,6 @@ const App: React.FC = () => {
   return (
     // Using HashRouter instead of BrowserRouter for GitHub Pages compat
     <HashRouter>
-      <TopNav />
       <Routes>
         <Route index element={<Home />} />
         <Route path="/rolling-magma" element={<RollingMagmaCalculator />} />
